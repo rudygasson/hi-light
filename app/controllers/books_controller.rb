c0c5edf64006799e814603e3f59c3c7624c3bcd7
Leonardo Campagnaro <campagnaro.leo@gmail.com>
2023-03-08 18:06:59 +0100

    book showpage wip

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,11 @@
 class BooksController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index ]
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "books#index"
+  resources :books, only: ['show']
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
