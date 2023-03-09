2e9f332aaf5629e614fa54c3dc371f69c533b74d
Leonardo Campagnaro <campagnaro.leo@gmail.com>
2023-03-09 06:26:31 +0100

    tags index created

--- app/controllers/highlights_controller.rb
+++ app/controllers/highlights_controller.rb
@@ -1,5 +1,18 @@
 class HighlightsController < ApplicationController
+  before_action :set_highlight, only: ['destroy']
+
   def index
     @highlights = Highlight.all
+  end
+
+  def destroy
+    @highlight.destroy
+    redirect_to book_path(book)
+  end
+
+  private
+
+  def set_highlight
+    @highlight = Highlight.find(params[:id])
   end
 end
