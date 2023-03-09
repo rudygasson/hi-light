fe107f0284921dd41882f99c8539ec859b9663eb
soline dziadzio <soline.dziadzio@gmail.com>
2023-03-09 10:31:34 +0100

    worked on tags, search and index

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,12 @@
 class TagsController < ApplicationController
+  def index
+    if params[:query].present?
+      sql_query = <<~SQL
+        tags.name @@ :query
+      SQL
+      @tags = Tag.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @tags = Tag.all
+    end
+  end
 end
