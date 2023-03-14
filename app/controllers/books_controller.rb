e46d5ad61f0e61a14af81847d158a2e4a1466786
Lucas Gruner <lucas.gruner@gmail.com>
2023-03-14 09:19:14 +0100

    empty tags greyed out, fixes on highlights and tags search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,8 @@ class BooksController < ApplicationController
       books.title @@ :query
       OR authors.name @@ :query
       SQL
-      @books = Book.joins(:author).where(sql_query, query: "%#{params[:query]}%", user: current_user)
+      @books = Book.joins(:author)
+        .where(sql_query, query: "%#{params[:query]}%") # TODO: REWORK AND current user
     else
       @books = Book.includes(:author).where(user: current_user)
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -64,17 +64,27 @@ quotes = [
   "It must be remembered that in becoming meat-eaters we had to chew, and therefore to taste, all this rich, unsuitable food. The carnivores - the great cats, the wolves and dogs, the crocodiles - merely tore their meat into pieces and swallowed it, careless of whether it was shoulder, rump steak, liver or tripes. We could not bolt our food.",
   "The best-fed, biggest and most enterprising people were undoubtedly those who followed the big cats",
   "This was all very well, but it did not solve the major problem or settle the host of minor inconveniences which are inevitable when the cat tribe is the ruling class. One of these is undoubtedly housing. Every ape-woman wants a decent place in which to bring up her family, a real home, snug, warm and, above all, dry; nobody, I fancy, will deny that basically this means a cave.",
-  "A dreadful quiet hung everywhere – except where a horde of flies had been zooming for days in obsessive parabolas."
+  "A dreadful quiet hung everywhere – except where a horde of flies had been zooming for days in obsessive parabolas.",
+  "This movement and change has been called Tao by the Chinese, yet in fact there is no movement, for the moment is the only reality and there is nothing beside it in relation to which it can be said to move. Thus it can be called at once the eternally moving and eternally resting",
+  "It is like trying to trace someone with whom you fell in love at first sight, and then lost touch; and you go back to the original place of meeting again and again, trying in vain to pick up the threads.",
+  "Importance is not measured by time, and change is a symptom of the presence of life",
+  "The message of the Eastern wisdom is that the forms of life are maya and therefore profoundly lacking in seriousness from the viewpoint of reality. For the world of form and illusion which the majority take to be the real world is none other than the play of the Spirit, or, as the Hindus have called it, the Dance of Shiva. He is enlightened who joins in this play knowing it as play, for man suffers only because he takes seriously what the gods made for fun.",
+  "So far as words, thoughts, ideas, and images are concerned, the doctrines of Buddhism and most forms of Hinduism are so negative and hopeless that they seem to be a sort of glorying in nihilism. They do not merely insist that human life is impermanent, that man has no immortal soul, and that in time every trace of our existence must vanish. They go on to indicate, as the wise man’s goal, a release from this transient life which seems to be no release at all—a state called nirvana, which may be translated “despair,” and the attainment of a metaphysical condition called shunyata, which is a voidness so void as to be neither existent nor nonexistent!",
+  "The unenlightened man keeps a tight hold on himself because he is afraid of losing himself; he can trust neither circumstances nor his own human nature; he is terrified of being genuine, of accepting himself as he is and tries to deceive himself into the belief that he is as he wishes to be. But these are the wishes, the desires that bind him, and it was such desires as these that the Buddha described as the cause of human misery.",
+  "Felix was large, as nursery bears go, and a rich golden-brown, to begin with, and he had an expression of thoughtful determination. He had been made in France, and that was how he came to be named Felix",
+  "To its critics, Canada is not a real country. It’s too large and too diverse and its history is too complicated. Even scholars concede that it’s a difficult country to know. At nearly 10 million square kilometres, it stretches across six time zones, east to west, from the Atlantic Ocean to the Pacific Ocean, and south to north, from its border with the United States to the High Arctic. (See Map 1) Only Russia is bigger.",
+  "North America was neither outside of history nor empty when Europe ‘discovered’ it in the late 15th century. It was home to some 500,000 people, belonging to numerous distinct cultures and speaking over fifty languages, but who were connected by elaborate trade routes criss-crossing the continent and who shared a similar cosmology. Despite different rituals, festivals, feasts, and rites of passage, Indigenous peoples believed that humans, animals, and plants enjoyed equal positions and interacted on equal terms, that everything had a spirit, including trees, rocks, and waterways, that the spirit world was as real as the material world, and that it was owed respect, even reverence, through prayers, offerings, and small daily practices.",
+  "The primitive Tarahumara Indians in Mexico’s remote Copper Canyon perform remarkable multi-day ultra-endurance running feats wearing nothing more than strips of rubber strapped to their feet."
 ]
 dates = ["Tuesday, 3 May 2022 19:24:53", "Saturday, 7 May 2022 19:48:03", "Monday, 9 May 2022 23:05:13"]
-30.times do
+quotes.each do |quote|
   location = (1..3000).to_a.sample
   book = Book.all.sample
 
   Highlight.create(
     user: book.user,
     book: book,
-    quote: quotes.sample,
+    quote: quote,
     page: (1..100).to_a.sample,
     location_start: location,
     location_end: location + (0..30).to_a.sample,
@@ -85,7 +95,7 @@ dates = ["Tuesday, 3 May 2022 19:24:53", "Saturday, 7 May 2022 19:48:03", "Monda
 end
 puts "---> #{Highlight.count} highlights in database."
 
-puts "Creating tags…"
+puts "Creating tags and hitags …"
 tags = [
   "university", "web dev", "friendship", "online", "work", "private", "school", "gardening", "passions",
   "skiing", "holidays", "online marketing", "mixology", "fashion", "furniture", "woodworking", "interior design",
@@ -100,22 +110,32 @@ colors = [
   "red", #"#E2A2BC", "#8F2D56"
   "orange", #"#F9B38A" "#F79256"
 ]
+
 User.all.each do |user|
   tags.sample(rand(1..tags.length)).each do |tag|
     Tag.create!(name: tag, user: user, color: colors.sample)
   end
-end
-puts "---> #{Tag.count} highlights in database."
-
-puts "Creating hi_tags…"
-User.all.each do |user|
   user.highlights.each do |highlight|
     num = rand(0..3)
     if num > 0
-      Tag.all.sample(num).each do |tag|
-        HiTag.create!(tag: tag, highlight: highlight)
+      user.tags.sample(num).each do |tag|
+        HiTag.create(tag: tag, highlight: highlight)
       end
     end
   end
 end
-puts "---> #{HiTag.count} hi_tags in database."
+puts "---> #{Tag.count} tags in database."
+puts "---> #{HiTag.count} hitags in database."
+
+# puts "Creating hi_tags…"
+# User.all.each do |user|
+#   user.highlights.each do |highlight|
+#     num = rand(0..3)
+#     if num > 0
+#       Tag.all.sample(num).each do |tag|
+#         HiTag.create!(tag: tag, highlight: highlight)
+#       end
+#     end
+#   end
+# end
+# puts "---> #{HiTag.count} hi_tags in database."
